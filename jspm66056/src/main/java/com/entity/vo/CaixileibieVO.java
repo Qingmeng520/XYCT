75371ecbe30be4425d4c1e3118cd9e81ce6a1438
Onlyone <83419318+Onlyone1116@users.noreply.github.com>
2022-06-27 19:17:07 +0800

    Add files via upload

---
+++ jspm66056/src/main/java/com/entity/vo/CaixileibieVO.java
@@ -0,0 +1,23 @@
+package com.entity.vo;
+
+import com.entity.CaixileibieEntity;
+
+import com.baomidou.mybatisplus.annotations.TableName;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import java.util.Date;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.io.Serializable;
+ 
+
+/**
+ * 菜系类别
+ * 手机端接口返回实体辅助类 
+ * （主要作用去除一些不必要的字段）
+
+ */
+public class CaixileibieVO  implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	 				
+}
