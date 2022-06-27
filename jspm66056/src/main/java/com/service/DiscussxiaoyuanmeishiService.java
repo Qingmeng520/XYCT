2a7b6fd22bfb419fafddf2b0be837d626d674881
Qingmeng520 <105829986+Qingmeng520@users.noreply.github.com>
2022-06-27 19:02:36 +0800

    Add files via upload

---
+++ jspm66056/src/main/java/com/service/DiscussxiaoyuanmeishiService.java
@@ -0,0 +1,32 @@
+package com.service;
+
+import com.baomidou.mybatisplus.mapper.Wrapper;
+import com.baomidou.mybatisplus.service.IService;
+import com.utils.PageUtils;
+import com.entity.DiscussxiaoyuanmeishiEntity;
+import java.util.List;
+import java.util.Map;
+import com.entity.vo.DiscussxiaoyuanmeishiVO;
+import org.apache.ibatis.annotations.Param;
+import com.entity.view.DiscussxiaoyuanmeishiView;
+
+
+/**
+ * 校园美食评论表
+ */
+public interface DiscussxiaoyuanmeishiService extends IService<DiscussxiaoyuanmeishiEntity> {
+
+    PageUtils queryPage(Map<String, Object> params);
+    
+   	List<DiscussxiaoyuanmeishiVO> selectListVO(Wrapper<DiscussxiaoyuanmeishiEntity> wrapper);
+   	
+   	DiscussxiaoyuanmeishiVO selectVO(@Param("ew") Wrapper<DiscussxiaoyuanmeishiEntity> wrapper);
+   	
+   	List<DiscussxiaoyuanmeishiView> selectListView(Wrapper<DiscussxiaoyuanmeishiEntity> wrapper);
+   	
+   	DiscussxiaoyuanmeishiView selectView(@Param("ew") Wrapper<DiscussxiaoyuanmeishiEntity> wrapper);
+   	
+   	PageUtils queryPage(Map<String, Object> params,Wrapper<DiscussxiaoyuanmeishiEntity> wrapper);
+   	
+}
+
---
+++ jspm66056/src/main/java/com/service/XiaoyuanmeishiService.java
@@ -0,0 +1,33 @@
+package com.service;
+
+import com.baomidou.mybatisplus.mapper.Wrapper;
+import com.baomidou.mybatisplus.service.IService;
+import com.utils.PageUtils;
+import com.entity.XiaoyuanmeishiEntity;
+import java.util.List;
+import java.util.Map;
+import com.entity.vo.XiaoyuanmeishiVO;
+import org.apache.ibatis.annotations.Param;
+import com.entity.view.XiaoyuanmeishiView;
+
+
+/**
+ * 校园美食
+
+ */
+public interface XiaoyuanmeishiService extends IService<XiaoyuanmeishiEntity> {
+
+    PageUtils queryPage(Map<String, Object> params);
+    
+   	List<XiaoyuanmeishiVO> selectListVO(Wrapper<XiaoyuanmeishiEntity> wrapper);
+   	
+   	XiaoyuanmeishiVO selectVO(@Param("ew") Wrapper<XiaoyuanmeishiEntity> wrapper);
+   	
+   	List<XiaoyuanmeishiView> selectListView(Wrapper<XiaoyuanmeishiEntity> wrapper);
+   	
+   	XiaoyuanmeishiView selectView(@Param("ew") Wrapper<XiaoyuanmeishiEntity> wrapper);
+   	
+   	PageUtils queryPage(Map<String, Object> params,Wrapper<XiaoyuanmeishiEntity> wrapper);
+   	
+}
+
